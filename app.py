a5e898900dcb775f1411cebd569a3f793e395cfa
ombekeellia431-boop <ombekeellia431@gmail.com>
2026-02-26 23:54:00 +0100

    Update and rename app (6).py to app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 from scipy.io import wavfile
-import moviepy.editor as mp # MoviePy is imported but not used for audio playback in this example
 import io
 import random
 
